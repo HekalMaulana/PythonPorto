a717c04c8fa266e63270076c14a0f91eab6ea0e4
HekalMaulana <116002257+HekalMaulana@users.noreply.github.com>
2023-06-24 21:09:23 +0800

    Band Generator Name
    ini adalah sebuah project untuk membuatkan nama dari sebuah groupband

---
+++ main.py
@@ -0,0 +1,19 @@
+#1. Create a greeting for your program.
+
+print("Welcome to Band's Generator Name")
+
+#2. Ask the user for the city that they grew up in.
+
+city_name = input("What's name of the city you grew up in ? ")
+
+#3. Ask the user for the name of a pet.
+
+pet_name = input("Whats yout pet's name ?  ")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+print("Your Band Name Could Be " + city_name + " " + pet_name)
+
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
