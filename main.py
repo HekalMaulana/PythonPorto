c3e5f0eaa83c9878f3d90168582dc74d770a6e93
HekalMaulana <116002257+HekalMaulana@users.noreply.github.com>
2023-06-25 18:35:40 +0800

    Get  Random Password
    Getting random password game

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
-# 🚨 Don't change the code below 👇
-row1 = ["⬜️","️⬜️","️⬜️"]
-row2 = ["⬜️","⬜️","️⬜️"]
-row3 = ["⬜️️","⬜️️","⬜️️"]
-map = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
-# 🚨 Don't change the code above 👆
-
 #Write your code below this row 👇
-horizontal = int(position[0])
-vertical = int(position[1])
-treasure_put = "X"
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-selected_vertical = map[vertical - 1]
-selected_vertical[horizontal - 1] = treasure_put
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+my_password = ""
+
+# get random letter
+for letter in range(0, nr_letters):
+    my_password += random.choice(letters)
+
+# get random symbos
+for symbol in range(0, nr_symbols):
+    my_password += random.choice(symbols)
+
+# get random number
+for number in range(0, nr_numbers):
+    my_password += random.choice(numbers)
+
+# Print random password without random list
+print(f"Not random password = {my_password}")
 
 
-#Write your code above this row 👆
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
-# 🚨 Don't change the code below 👇
-print(f"{row1}\n{row2}\n{row3}")
+# Memasukan random password ke dalam list
+my_password_list = []
+for n in range(0, len(my_password)):
+    my_password_list += my_password[n]
 
+# Mengacak urutan sebuah list
+random.shuffle(my_password_list)
+
+# Memasukan tiap list ke dalam sebuah string
+my_random_password = ""
+for random_pasword in my_password_list:
+    my_random_password += random_pasword
+
+print(f"\nRandom password = {my_random_password}")
